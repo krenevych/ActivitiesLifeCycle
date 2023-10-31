41afd880de5cf87b9530fb25af90a7d76eebc56b
Andrii Krenevych <akrenevych@gmail.com>
2023-10-31 13:53:06 +0200

    Activity LifeCycle

--- app/src/main/java/com/example/activitieslifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitieslifecycle/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.example.activitieslifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button : Button
+    private lateinit var name: EditText
+    private lateinit var age: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate")
+
+        button = findViewById(R.id.button)
+        name = findViewById(R.id.editTextName)
+        age = findViewById(R.id.editTextAge)
+
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+
+            val textName: String = name.text.toString()
+            val textAge: Int = age.text.toString().toInt()
+
+            intent.putExtra(EXTRA_NAME, textName)
+            intent.putExtra(EXTRA_AGE, textAge)
+
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    companion object {
+        const val TAG = "CCCCCC"
+        const val EXTRA_NAME = "name"
+        const val EXTRA_AGE = "age"
     }
 }
--- app/src/main/java/com/example/activitieslifecycle/SecondActivity.kt
+++ app/src/main/java/com/example/activitieslifecycle/SecondActivity.kt
@@ -1,11 +1,35 @@
 package com.example.activitieslifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.example.activitieslifecycle.MainActivity.Companion.EXTRA_AGE
+import com.example.activitieslifecycle.MainActivity.Companion.EXTRA_NAME
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var buttonReturnToFirstActivity: Button
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        buttonReturnToFirstActivity = findViewById(R.id.button2)
+        buttonReturnToFirstActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        textView = findViewById(R.id.secActivitytextView)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
+
+        textView.text = "name = $name, age = $age"
+
+        Log.d("XXXX", "name = $name, age = $age")
     }
 }
